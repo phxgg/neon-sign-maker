5434f075500551691bd443ab7fc1a03fe408aed8
phxgg <stamatis.1702@gmail.com>
2024-04-30 00:54:52 +0300

    feat: memoize the Heading component so it does not rerender on every input change

---
+++ src/components/heading.tsx
@@ -0,0 +1,33 @@
+import { siteConfig } from '@/config/siteConfig';
+
+import { SparklesCore } from './ui/sparkles';
+
+export function Heading() {
+  return (
+    <div className="w-full bg-background flex flex-col items-center justify-center overflow-hidden rounded-md">
+      <h1 className="md:text-7xl text-3xl lg:text-9xl font-bold text-center relative z-20">
+        {siteConfig.title}
+      </h1>
+      <div className="w-[40rem] relative">
+        {/* Gradients */}
+        <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
+        <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
+        <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
+        <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
+
+        {/* Core component */}
+        <SparklesCore
+          background="transparent"
+          minSize={0.4}
+          maxSize={1}
+          particleDensity={1200}
+          className="w-full h-full"
+          particleColor="#FFFFFF"
+        />
+
+        {/* Radial Gradient to prevent sharp edges */}
+        <div className="absolute inset-0 w-full h-full bg-background [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
+      </div>
+    </div>
+  );
+}
--- src/components/neon-sign-maker.tsx
+++ src/components/neon-sign-maker.tsx
@@ -1,12 +1,13 @@
 'use client';
 
-import { useState } from 'react';
+import { memo, useState } from 'react';
 import Image from 'next/image';
 import { RadioGroup } from '@radix-ui/react-radio-group';
 
 import { siteConfig } from '@/config/siteConfig';
 import { cn } from '@/lib/utils';
 
+import { Heading as OriginalHeading } from './heading';
 import { Input } from './ui/input';
 import { Label } from './ui/label';
 import { RadioGroupItem } from './ui/radio-group';
@@ -19,7 +20,8 @@ import {
   SelectTrigger,
   SelectValue,
 } from './ui/select';
-import { SparklesCore } from './ui/sparkles';
+
+const Heading = memo(OriginalHeading);
 
 export function NeonSignMaker() {
   const [font, setFont] = useState(siteConfig.fonts.capriola);
@@ -31,34 +33,7 @@ export function NeonSignMaker() {
       <section className="container mx-auto py-6 md:py-12">
         <div className="mx-4 space-y-6 md:mx-6 lg:mx-0">
           <div className="space-y-2 text-center">
-            {/* START: Sparks */}
-            <div className="w-full bg-background flex flex-col items-center justify-center overflow-hidden rounded-md">
-              <h1 className="md:text-7xl text-3xl lg:text-9xl font-bold text-center relative z-20">
-                NeonSpark
-              </h1>
-              <div className="w-[40rem] relative">
-                {/* Gradients */}
-                <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
-                <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
-                <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
-                <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
-
-                {/* Core component */}
-                <SparklesCore
-                  background="transparent"
-                  minSize={0.4}
-                  maxSize={1}
-                  particleDensity={1200}
-                  className="w-full h-full"
-                  particleColor="#FFFFFF"
-                />
-
-                {/* Radial Gradient to prevent sharp edges */}
-                <div className="absolute inset-0 w-full h-full bg-background [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
-              </div>
-            </div>
-            {/* END: Sparks */}
-
+            <Heading />
             <p className="max-w-[700px] md:text-xl">
               Design your own unique neon sign with our easy-to-use tool. Choose
               your text, font, and size, then see the preview come to life.
